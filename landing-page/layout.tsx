8d7f2abd4497814a8545bb47dd0ef3882ba45e8a
srivathsav004 <srivathsavgangavaram004@gmail.com>
2025-09-25 00:34:43 +0530

    analytics

--- landing-page/layout.tsx
+++ landing-page/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '../app/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +20,7 @@ export default function LandingLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
